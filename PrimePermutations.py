b57b24baaa252eb4c60bc0aa99709f82860ffb84
Jasper Rou <105650583+jgrou@users.noreply.github.com>
2024-03-19 13:31:12 +0100

    remove math

--- PrimePermutations.py
+++ PrimePermutations.py
@@ -1,9 +1,7 @@
-import math
-
 ans = []
 
 def Prime(n):
-    for i in range(2,int(math.sqrt(n)+1)):
+    for i in range(2, int(n**0.5+1)):
         if n%i == 0:
             return False
     return True
