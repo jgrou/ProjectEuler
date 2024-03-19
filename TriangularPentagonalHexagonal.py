b3d6ceb7d298e65c568544b946605e9beebf273b
Jasper Rou <105650583+jgrou@users.noreply.github.com>
2024-03-19 14:45:26 +0100

    remove math

--- TriangularPentagonalHexagonal.py
+++ TriangularPentagonalHexagonal.py
@@ -1,11 +1,9 @@
-import math
-
 n = 286
 
 while True:
     Tn = n * (n+1) / 2 # Compute next triangular number
-    if ((math.sqrt(1 + 24 * Tn) + 1) / 6) % 1 == 0: # Check if pentagonal
-        if ((math.sqrt(1 + 8 * Tn) + 1) / 4) % 1 == 0: # Check if hexagonal
+    if (((1 + 24 * Tn)**0.5 + 1) / 6) % 1 == 0: # Check if pentagonal
+        if (((1 + 8 * Tn)**0.5 + 1) / 4) % 1 == 0: # Check if hexagonal
             ans = Tn
             break
     n += 1
