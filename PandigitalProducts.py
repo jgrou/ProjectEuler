b59bc43045f13e73301f5c93bdb25f12903c7c5b
Jasper Rou <105650583+jgrou@users.noreply.github.com>
2024-03-19 13:29:05 +0100

    remove math

--- PandigitalProducts.py
+++ PandigitalProducts.py
@@ -1,10 +1,8 @@
-import math
-
 PandigitalNumber = []
 integers = [str(i) for i in range(1,10)]
 
 for n in range(10000): # Impossible to write a five digit number as product of two two-digit numbers
-    for i in range(1,int(math.sqrt(n))+1):
+    for i in range(1, int(n**0.5)+1):
         if n%i ==0:
             string = str(n) + str(i) + str(int(n/i))
             if len(string) == 9:
