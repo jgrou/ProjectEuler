e75247864c52c931b74d0d3a158d2d8d1770e9c7
Jasper Rou <jasperrou@gmail.com>
2024-06-24 09:33:16 +0200

    Update CountingDigits.py

--- CountingDigits.py
+++ CountingDigits.py
@@ -1,32 +1,37 @@
-def count_at_place(digit_to_count, limit, this_digit, this_place, base=10):
+def CountAtPlace(digit, limit, this_digit, this_place, base=10):
     lpv = base**this_place      # Larger of two relevant place values
     spv = base**(this_place-1)  # Smaller of two relevant place values
     until_target_digit = (limit // lpv) * spv
-    if this_digit < digit_to_count:
+    
+    if this_digit < digit:
         return until_target_digit
-    elif this_digit > digit_to_count:
+    elif this_digit > digit:
         return until_target_digit + spv
     else:
         return until_target_digit + (limit % spv) + 1
 
-def count_by_digits(digit_to_count, limit, base=10):
+def CountByDigits(digit, limit, base=10):
     limit_digits = strbase(limit, base)
     length = len(limit_digits)
-    counts_by_digit = [count_at_place(digit_to_count, limit, this_digit, length-i, base=base) for i, this_digit in enumerate(limit_digits)]
-    return sum(counts_by_digit)
+    counts = [CountAtPlace(digit, limit, this_digit, length-i, base=base) for i, this_digit in enumerate(limit_digits)]
+    return sum(counts)
 
 def strbase(n, b):
-    # Convert a python numeric value n to a base b list of numeric values. E.g. convert 23 to [1, 11] for base b=12.
-    return [n] if n < b else strbase(n // b, b) + [n % b]
+    # Convert a python numeric value n to a base b list of numeric values
+    if n < b:
+        return [n] 
+    else:
+        return strbase(n // b, b) + [n % b]
 
-def unbounded_binary_search(digit=1, base=10, safeleft=2, width=1):
-    fn = lambda x: count_by_digits(digit, x, base=base)
+def UnboundedBinarySearch(digit=1, base=10, safeleft=2, width=1):
     eq = None
     limit = digit * base**base
+    
     while safeleft <= limit and eq is None:
         upper = safeleft + width
-        fnsl = fn(safeleft)
-        if eq is None and width == 1 and fn(upper) == upper:
+        fnsl = CountByDigits(digit, safeleft, base=base)
+        
+        if eq is None and width == 1 and CountByDigits(digit, upper, base=base) == upper:
             eq = upper
             safeleft, width = upper, 1
             continue
@@ -35,7 +40,7 @@ def unbounded_binary_search(digit=1, base=10, safeleft=2, width=1):
             # between safeleft and fn(safeleft) - 1 have the digit of interest,
             # fn(safeleft) will still remain larger, so we can skip ahead a bit.
             safeleft, width = fnsl - 1, 1
-        elif width == 1 or fn(upper) < safeleft:
+        elif width == 1 or CountByDigits(digit, upper, base=base) < safeleft:
             safeleft, width = upper, width * 2
         else:
             safeleft, width = safeleft, width // 2
@@ -47,4 +52,4 @@ for digit in range(1,10):
     x = 0
     while x is not None:
         s += x
-        x = unbounded_binary_search(digit=digit, safeleft=x)
+        x = UnboundedBinarySearch(digit=digit, safeleft=x)
