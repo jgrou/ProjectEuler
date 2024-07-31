767a5d6218317a877e3cc0687b0cd7b474d1b065
Jasper Rou <jasperrou@gmail.com>
2024-07-31 15:08:40 +0200

    Update SquareSumOfTheDigitalSquares.py
    Very nice

--- SquareSumOfTheDigitalSquares.py
+++ SquareSumOfTheDigitalSquares.py
@@ -1,96 +1,45 @@
 import itertools
+import math
 
 squares = [i**2 for i in range(10)]
-limit = 20
-NoOfDigits = 9
 ans = 0
 
-# Yield all permutations of size NoOfDigits when there are doubles
-def YieldAllPermutations(s, NoOfDigits): 
+def CountOccurrencesAtEachPosition(partition):
+    '''For a given partition [n1,n2,...] of 20, count how often the digit that occurs n1 times occurs at each position'''
+    res = {}
     
-    def YieldAllPermutationsFast(s, l):
-        if len(s) < TotalDigits + 1 - NoOfDigits:
-            yield l, s
-        else:
-            uset = set()
-     
-            for i in range(len(s)):
-                if s[i] in uset:
-                    continue
-                else:
-                    uset.add(s[i])
-         
-                temp = ""
-                if (i < len(s) - 1):
-                    temp = s[:i] + s[i + 1:]
-                else:
-                    temp = s[:i]
-         
-                yield from YieldAllPermutationsFast(temp, l + s[i])
-                
-    TotalDigits = len(s)
-    yield from YieldAllPermutationsFast(s, '')
+    for i,n in enumerate(partition):
+        new_partition = partition.copy()
+        new_partition[i] -= 1
+        possibilities = 1
+        m = 19
+        
+        for k in new_partition:
+            possibilities *= math.comb(m, k)
+            m -= k
+            
+        res[n] = possibilities
+        
+    return res
 
-#Python code for the above approach
-class Solution:
-	def __init__(self):
-		self.temp = []
-
-	def solve(self, a, v, idx, s, n):
-		# first base case if sum=n we can store vector in a
-		# vector
-		if s == n:
-			v.append(self.temp.copy())
-			return
-
-		# if idx < 0 return
-		if idx < 0:
-			return
-
-		# not take condition
-		self.solve(a, v, idx-1, s, n)
-		if s < n:
-			self.temp.append(a[idx])
-			# this is main condition where we can take one
-			# element many times
-			self.solve(a, v, idx, s+a[idx], n)
-			self.temp.pop()
-
-	def UniquePartitions(self, n):
-		a = [i for i in range(1, n+1)]
-		v = []
-		# call solve to get answer
-		self.solve(a, v, n-1, 0, n)
-		v.reverse()
-		return v
-
-sol = Solution()
-dic = {}
-UniquePartitions = sol.UniquePartitions(11)
-for partition in UniquePartitions:
-    string = ''
-    for i, n in enumerate(partition):
-        string += n*str(i)
-    if len(string) <= 11:
-        dic[tuple(partition)] = sum(1 for _ in YieldAllPermutations(string, len(string)))
-
-def NumberOfCombinations(string):
-    '''For a given set of digits, the number of different permutations we can make only depends on the number of 
-    occurences, independent of the value of the digit. So we first make a dicitionary for each permutation and then
-    we can precompute all values'''
-    partition = []
+# For each combination of 20 digits for which the sum of the squares is a square:
+    # Count how often each unique digits occurs at every position
+    # Add this n
     
-    for digit in range(10):
-        occurs = string.count(str(digit))
-        if occurs > 0:
-            partition.append(occurs)
-    partition.sort(reverse=True)
-    return dic[tuple(partition)]
-
-for combination in itertools.combinations_with_replacement(squares, limit):
+for combination in itertools.combinations_with_replacement(squares, 20):
     s = sum(combination)
     if int(s**0.5)**2 == s:
-        digits = ''.join(map(str,[int(x**0.5) for x in combination]))
-        for permutation, rest in YieldAllPermutations(digits,9): # Only need last nine digits
-            ans += NumberOfCombinations(rest) * int(permutation)
+        digits = [int(x**0.5) for x in combination]
+        unique_digits = set(digits)
+        counter = {}
+        
+        for digit in unique_digits:
+            occurs = digits.count(digit)
+            if occurs > 0:
+                counter[digit] = occurs
+        
+        occurances = CountOccurrencesAtEachPosition(list(counter.values()))
+        
+        for k in unique_digits:
+            ans += occurances[counter[k]] * int(9*str(k))
             ans %= 10**9
