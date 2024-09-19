26f5d101bbdaa1f1440eaa6c12a68f20f2439f4c
Jasper Rou <j.g.rou@tudelft.nl>
2024-09-19 16:27:24 +0200

    Update SwappingCounters.py
    Works!

--- SwappingCounters.py
+++ SwappingCounters.py
@@ -1,15 +1,14 @@
 # Pattern: slide, 1 hop, slide, 2 hops, slide, ..., slide, n hops, slide, n-1 hops, slide, ..., slide, 1 hop, slide
+# M(n) = n**2 + 2*n = k**2 + k for some k
+# For the correct n, n+1 follows A006452: a(n) = 6*a(n-2) - a(n-4), with the first four: 1,1,2,4
 
 limit = 40
-s = 0
-ans = 0
-n = 1
 
-while s < limit:
-    M = n**2 + 2*n
-    k = (-1 + (1 + 8 * M)**0.5) / 2
-    k = round(k)
-    if k**2 + k == 2 * M:
-        s += 1
-        ans += n
-    n += 1
+a = (limit + 2) * [1]
+a[2] = 2
+a[3] = 4
+
+for n in range(4, limit+2):
+    a[n] = 6 * a[n-2] - a[n-4]
+    
+ans = sum(a[2:]) - limit
