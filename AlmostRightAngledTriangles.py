4c172c352d0580383a6dc33cd51073d8fffab2f2
Jasper Rou <jasperrou@gmail.com>
2024-09-27 15:30:43 +0200

    Update AlmostRightAngledTriangles.py
    Too slow

--- AlmostRightAngledTriangles.py
+++ AlmostRightAngledTriangles.py
@@ -1,13 +1,15 @@
 import math
-limit = 250
+limit = 25_000
 ans = (limit - 1) // 2 # b=c, a=1
 
-for c in range(1, limit//2 + 1):
+max_c = limit // 2 # a + b >=c
+
+for c in range(1, max_c + 1):
     min_b = ((c**2 +1)/2)**0.5 # b >= a
     
     for b in range(math.ceil(min_b), c):
         a = round((c**2 - b**2 + 1)**0.5)
         if a**2 + b**2 == c**2 + 1:
             if a + b + c <= limit:
-                print(a,b,c)
+                #print(a,b,c)
                 ans += 1
