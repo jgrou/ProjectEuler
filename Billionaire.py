376185bd2d662fe177946bc6885d727e3176cceb
Jasper Rou <j.g.rou@tudelft.nl>
2024-10-08 12:47:52 +0200

    Update Billionaire.py
    I was stupid, gain is more than loss. Works now

--- Billionaire.py
+++ Billionaire.py
@@ -1,17 +1,33 @@
 import math
-import matplotlib.pyplot as plt
 
 def ExpectedCapital(f, n_flips=1000, initial_captial=1.0):
     res = 0
     for n_heads in range(n_flips+1):
-        capital = (1+f)**n_heads * (1-f)**(n_flips - n_heads) * initial_captial
+        capital = initial_captial
+        n_tails = n_flips - n_heads
+        
+        if n_tails > n_heads:
+            for flip in range(n_heads):
+                capital *= (1.0 + 2 * f)
+                capital *= (1.0 - f)
+            for flip in range(n_heads, n_tails):
+                capital *= (1 + 2 * f)
+                if capital > 10**9:
+                    break
+        else:
+            for flip in range(n_tails):
+                capital *= (1.0 + 2 * f)
+                capital *= (1.0 - f)
+            for flip in range(n_tails, n_heads):
+                capital *= (1 - f)
+                if capital < 10**9:
+                    break
+                
         if capital >= 10**9:
             res += math.comb(n_flips, n_heads) 
     return res / 2**n_flips
 
-def function(f):
-    return - ExpectedCapital(f)
-
-x = [0.15 + i*0.001 for i in range(101)]
+# With a bit of guessing:
+x = [0.01*i for i in range(101)]
 y = [ExpectedCapital(xi) for xi in x]
-plt.plot(x,y)
+ans = round(max(y),12) 
